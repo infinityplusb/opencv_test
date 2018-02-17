b940c393f17d689e3991d5638c21985521e775f9
infinityplusb <brian@infinityplusb.com>
2018-02-17 23:35:15 +1100

    this causes compiler error

--- core/cvstd.d
+++ core/cvstd.d
@@ -5,6 +5,10 @@ module opencv_test.core.cvstd.d;
 modmap (C++) "cvstd.hpp" ;
 
 import (C++) cv._ ;
+// import (C++) detail._ ;
+
+//import (C++) DefaultDeleter;
+
 //alias CV_EXPORTS = string;
 
 // import (C++) cv.CV_EXPORTS ;
